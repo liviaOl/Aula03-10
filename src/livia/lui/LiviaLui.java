4599af3b3f9f1b60f2683f13e8073e59008ad2c9
liviaOl <43821580+liviaOl@users.noreply.github.com>
2018-10-03 12:40:26 -0400

    Update LiviaLui.java

--- src/livia/lui/LiviaLui.java
+++ src/livia/lui/LiviaLui.java
@@ -6,6 +6,7 @@ public class LiviaLui {
 
     public static void main(String[] args) {
         System.out.println("today is 03/10");
+        System.out.println("sai cocozinho do butico do lui");
     }
     
 }
